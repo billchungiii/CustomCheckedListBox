89739ce8a5510736893107b632f00e843d8bfd67
Bill Chung <bill_chungiii@hotmail.com>
2018-12-04 01:16:29 +0800

    加入專案檔案。

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomCheckedListBox
+{
+    public partial class Form1 : Form
+    {
+        MyCheckedListbox boxes;
+        public Form1()
+        {
+            InitializeComponent();
+            boxes = new MyCheckedListbox();
+            for (int i = 0; i < 10; i++)
+            {
+                boxes.Items.Add($"A0{i}");
+            }
+
+            panel2.Controls.Add(boxes);
+            boxes.Dock = DockStyle.Fill; 
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            boxes.SetVisualState(2, false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            boxes.SetVisualState(2, true);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            boxes.SetVisualState("A08", false);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            boxes.SetVisualState("A08", true);
+        }
+    }
+}
---
+++ MyCheckedListbox.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace CustomCheckedListBox
+{
+    public class MyCheckedListbox : CheckedListBox
+    {
+        private List<DisabledItem> _disabledItems;
+
+        public MyCheckedListbox()
+        {
+            _disabledItems = new List<DisabledItem>();
+
+        }
+        public void SetVisualState(int index, bool enabled)
+        {
+            if (index < 0 || index >= this.Items.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            SetVisualState(Items[index], enabled);
+        }
+
+
+        public void SetVisualState(object item, bool enabled)
+        {
+            if (!Items.Contains(item))
+            { throw new ArgumentException(); }
+
+            var targetItem = _disabledItems.FirstOrDefault((x) => x.Item == item);
+            if (enabled)
+            {
+                RemoveDisabledItem(targetItem);
+
+            }
+            else
+            {
+                AddDisabledItem(item, targetItem);
+            }
+
+            this.Refresh();
+
+        }
+
+        private void AddDisabledItem(object item, DisabledItem targetItem)
+        {
+            if (targetItem == null)
+            {
+                bool isChecked = this.CheckedItems.Contains(item);
+                var addItem = new DisabledItem { Item = item, Checked = isChecked };
+                _disabledItems.Add(addItem);
+            }
+        }
+
+        private void RemoveDisabledItem(DisabledItem targetItem)
+        {
+            if (targetItem != null)
+            {
+                _disabledItems.Remove(targetItem);
+            }
+        }
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            var item = Items[e.Index];
+            var targetItem = _disabledItems.FirstOrDefault((x) => x.Item.Equals(item));
+            if (targetItem != null)
+            {
+                CheckBoxState state = state = CheckBoxState.UncheckedDisabled;
+                if (targetItem.Checked)
+                {
+                    state = CheckBoxState.CheckedDisabled;
+                }
+                Size glyphSize = CheckBoxRenderer.GetGlyphSize(e.Graphics, state);
+                CheckBoxRenderer.DrawCheckBox(e.Graphics, new Point(e.Bounds.X + 1, e.Bounds.Y + 1),
+                    new Rectangle(new Point(e.Bounds.X + glyphSize.Width + 3, e.Bounds.Y), new Size(e.Bounds.Width - glyphSize.Width, e.Bounds.Height)),
+                    item.ToString(), this.Font, TextFormatFlags.Left, false, state);
+            }
+            else
+            {
+                base.OnDrawItem(e);
+            }
+        }
+        private class DisabledItem
+        {
+            internal object Item { get; set; }
+            internal bool Checked { get; set; }
+        }
+    }
+}
